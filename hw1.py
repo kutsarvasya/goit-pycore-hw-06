e02cc933c5c7c7f50d229b1542c17840a9d3f983
Vasyl Kutsar <vkutsar89@gmail.com>
2025-03-29 20:21:57 +0100

    update Record

--- hw1.py
+++ hw1.py
@@ -1,5 +1,5 @@
 from collections import UserDict
-from math import e
+
 
 class Field:
     def __init__(self, value):
@@ -7,6 +7,7 @@ class Field:
 
     def __str__(self):
         return str(self.value)
+
 
 class Name(Field):
     def __init__(self, value):
@@ -18,29 +19,35 @@ class Phone(Field):
         if not self.is_valid(value):
             raise ValueError("the phone number is not correct")
         super().__init__(value)
+
     def is_valid(self, phone):
-        return len(phone)==10 and phone.isdigit()
+        return len(phone) == 10 and phone.isdigit()
 
-
-         
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
-    def edit_phone(self,old_phone, new_phone):
+    def edit_phone(self, old_phone, new_phone):
         for phone in self.phones:
             if phone.value == old_phone:
                 phone.value = new_phone
-    
 
-    def add_phone(self,value):
+    def add_phone(self, value):
         try:
             self.phones.append(Phone(value))
         except ValueError as e:
-             print(e)
-             
+            print(e)
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                print(f"Phone number {phone} removed.")
+                return
+        print(f"Phone number {phone} not found.")
+
     def find_phone(self, phone):
         for p in self.phones:
             if p.value == phone:
@@ -57,8 +64,7 @@ class AddressBook(UserDict):
     def find(self, name):
         record = self.data.get(name)
         if record:
-           return record
-
+            return record
 
     def delete(self, name):
         if name in self.data:
@@ -68,42 +74,38 @@ class AddressBook(UserDict):
             print(f"contact {name} not removed.")
 
 
-        
-
-
 # Створення нової адресної книги
 book = AddressBook()
 
-    # Створення запису для John
+# Створення запису для John
 john_record = Record("John")
 john_record.add_phone("1234567890")
 john_record.add_phone("5555555555")
 
-    # Додавання запису John до адресної книги
+# Додавання запису John до адресної книги
 book.add_record(john_record)
 
-    # Створення та додавання нового запису для Jane
+# Створення та додавання нового запису для Jane
 jane_record = Record("Jane")
 jane_record.add_phone("9876543210")
 book.add_record(jane_record)
 
- 
 
 #     # Знаходження та редагування телефону для John
 john = book.find("John")
 john.edit_phone("1234567890", "1112223333")
+
+john.remove_phone('1112223333')
 print(john)
 
 
+# Виведення: Contact name: John, phones: 1112223333; 5555555555
 
-
- # Виведення: Contact name: John, phones: 1112223333; 5555555555
-
-  # Пошук конкретного телефону у записі John
+# Пошук конкретного телефону у записі John
 found_phone = john.find_phone("5555555555")
 print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
 
-    # Видалення запису Jane
+# Видалення запису Jane
 book.delete("Jane")
 for name, record in book.data.items():
-        print(record)
+    print(record)
